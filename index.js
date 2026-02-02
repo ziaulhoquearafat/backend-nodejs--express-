bef181b431d3c7ca671c85c9e1209c433d2c643e
ziaulhoquearafat <mdarafat3167@gmail.com>
2026-02-02 19:41:50 +0600

    test gitignore

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const user = arafat;
+const express = require("express");
+const cors = require('cors')
+const app = express();
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req,res)=>{
+    res.send('Simple Crud Servert');
+})
+
+app.listen(port, ()=>{
+    console.log(`Serve is running on port ${port}`)
+})
+
