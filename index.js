1f276b55f7e1dd253f8fe9b59d5e2c39c4bc7b40
ziaulhoquearafat <mdarafat3167@gmail.com>
2026-02-03 17:52:40 +0600

    fix: update response message and restructure user routes

--- index.js
+++ index.js
@@ -8,7 +8,21 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (req,res)=>{
-    res.send('Simple Crud Servert');
+    res.send('Back to the code');
+})
+
+const users = [
+    {id:1, name:'Sabana', gmail:'Sabana@gmail.com'},
+    {id:2, name:'Sabila Noor', gmail:'Sabilanoor@gmail.com'},
+    {id:3, name:'Sabnoor', gmail:'Sabnoor@gmail.com'}
+]
+
+app.get('/users', (req,res)=>{
+    res.send(users)
+})
+
+app.post('/users',(req,res)=>{
+    console.log('post method hit')
 })
 
 app.listen(port, ()=>{
