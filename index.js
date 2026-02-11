ac82fee91b5ae1c038d4cfc626aa0a7fa37fa909
ziaulhoquearafat <mdarafat3167@gmail.com>
2026-02-11 07:39:27 +0600

    mongodb atlas implement

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require("cors")
 const app = express()
 const port = process.env.PORT || 3000
@@ -7,10 +8,36 @@ const port = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
 
+const uri = "mongodb://localhost:27017"
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
 app.get('/', (req,res)=>{
     res.send('Server is running')
 })
 
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`)
 })
+
