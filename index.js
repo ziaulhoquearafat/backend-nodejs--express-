c5c6b1de5e57e6f1f572d2f187fff44f95497dc6
ziaulhoquearafat <mdarafat3167@gmail.com>
2026-02-17 21:57:45 +0600

    post and get method implement

--- index.js
+++ index.js
@@ -1,35 +1,52 @@
-const express = require("express")
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const cors = require("cors")
-const app = express()
-const port = process.env.PORT || 3000
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
 
-// middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-const uri = "mongodb://localhost:27017"
+const uri = "mongodb://localhost:27017";
 
-
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-app.get('/', (req,res)=>{
-    res.send('Server is running')
-})
+app.get("/", (req, res) => {
+  res.send(`Server Is Running`);
+});
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const userDB = client.db("userDB");
+    const usersCollection = userDB.collection("users");
+
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      console.log(newUser);
+      const result = await usersCollection.insertOne(newUser);
+      res.send(result);
+    });
+
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -37,7 +54,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`)
-})
-
+app.listen(port, () => {
+  console.log(`Server is Running On port, ${port}`);
+});
